ea4c0964b5452473de80ed020e54a5887db311fa
niv b <nivb52@gmail.com>
2022-07-02 23:15:19 +0300

    solved 02 + add starter file

---
+++ src/exercise/02.extra.js
@@ -0,0 +1,48 @@
+// Compound Components
+// http://localhost:3000/isolated/exercise/02.js
+
+import * as React from 'react'
+import {Switch} from '../switch'
+
+function Toggle({children}) {
+  const [on, setOn] = React.useState(false)
+  const toggle = () => setOn(!on)
+
+  return React.Children.map(children, child => {
+    return typeof child.type === 'string'
+      ? child
+      : React.cloneElement(child, {
+          on,
+          toggle,
+        })
+  })
+}
+
+// Accepts `on` and `children` props and returns `children` if `on` is true
+const ToggleOn = ({on, children}) => (on ? children : null)
+
+// Accepts `on` and `children` props and returns `children` if `on` is false
+const ToggleOff = ({on, children}) => (on ? null : children)
+
+// Accepts `on` and `toggle` props and returns the <Switch /> with those props.
+const ToggleButton = ({on, toggle}) => <Switch on={on} onClick={toggle} /> //<Switch on={on} toggle={toggle} />
+
+function App() {
+  return (
+    <div>
+      <Toggle>
+        <ToggleOn>The button is on</ToggleOn>
+        <ToggleOff>The button is off</ToggleOff>
+        <span>Hello</span>
+        <ToggleButton />
+      </Toggle>
+    </div>
+  )
+}
+
+export default App
+
+/*
+eslint
+  no-unused-vars: "off",
+*/
