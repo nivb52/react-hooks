3108b62acecf08af5c89bff0db787b5894d78b7f
niv b <nivb52@gmail.com>
2022-07-11 01:14:03 +0300

    solve - prop getters

---
+++ src/exercise/04.extra.js
@@ -0,0 +1,52 @@
+// Prop Collections and Getters
+// http://localhost:3000/isolated/exercise/04.js
+
+import * as React from 'react'
+import {Switch} from '../switch'
+
+function useToggle() {
+  const [on, setOn] = React.useState(false)
+  const toggle = () => setOn(!on)
+
+  function getTogglerProps({onClick, ...customProps} = {}) {
+    return {
+      'aria-pressed': on,
+      on,
+      toggle,
+      onClick: () => {
+        if (typeof onClick === 'function') {
+          onClick()
+        }
+        toggle()
+      },
+      ...customProps,
+    }
+  }
+  return {on, getTogglerProps}
+}
+
+function App() {
+  const {on, getTogglerProps} = useToggle()
+  return (
+    <div>
+      <Switch {...getTogglerProps({on})} />
+      <hr />
+      <button
+        {...getTogglerProps({
+          'aria-label': 'custom-button',
+          onClick: () => console.info('onButtonClick'),
+          id: 'custom-button-id',
+        })}
+      >
+        {on ? 'on' : 'off'}
+      </button>
+    </div>
+  )
+}
+
+export default App
+
+/*
+eslint
+  no-unused-vars: "off",
+*/
